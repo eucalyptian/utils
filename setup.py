6e50edad2d6657cb45dd64a7da1913eb83d57fa2
saeid@9fnb <140948642+eucalyptian@users.noreply.github.com>
2026-05-09 21:09:49 +0330

    Update setup.py
    Changed version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="eucalyptian-utils",
-    version="0.1.0",
+    version="0.2.0",
     description="Utility functions by eucalyptian",
     author="Saeid Badamchizadeh",
     url="https://github.com/eucalyptian/utils",
